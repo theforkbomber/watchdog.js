4aae2a69b072d0b53d3b08dfa94115b96fc9196d
Mohamed Ali <theforkbomber@gmail.com>
2018-11-22 18:32:49 +0000

    checking

--- cogs/serverinfo.py
+++ cogs/serverinfo.py
@@ -96,6 +96,8 @@ platform.version(),
                     msg = await self.bot.send_message(x,str(ctx.message.author.name)+" has pinged you because:\n`"+reason+"`\nReact with the 🚫 emote to give "+str(ctx.message.author.name)+" a detention.")
                     msgs.append(msg)
                     await self.bot.add_reaction(emoji="🚫", message=msg)
+                print(msgs)
+                print(msg)
                 waited = await self.bot.wait_for_reaction(emoji="🚫",check = check)
                 await self.bot.send_message(x, str(ctx.message.author.name)+" has been detained by "+str(waited.user.name))
                 for role in server.roles:
