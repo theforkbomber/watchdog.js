fe3ca121018e406ead69a1692d8924d70594f404
Mohamed Ali <theforkbomber@gmail.com>
2018-12-06 17:19:43 +0000

    Up and running

--- main.py
+++ main.py
@@ -588,9 +588,9 @@ async def on_message(message):
             msg = msg +"\n"+message.embeds[0]["footer"]["text"]
         except:
             pass
-    # cursor.execute("INSERT INTO logs (todisplay, id, details, time, channel)VALUES(%s,%s,%s,%s) RETURNING id;", (str(msg), str(message.id), str(details), message.timestamp, str(message.channel.id)))
-    # db.commit()
-    # db.close()
+    cursor.execute("INSERT INTO logs (todisplay, id, details, time, channel)VALUES(%s,%s,%s,%s) RETURNING id;", (str(msg), str(message.id), str(details), message.timestamp, str(message.channel.id)))
+    db.commit()
+    db.close()
     if message.author.bot == True:
         return
     
@@ -750,52 +750,52 @@ async def translate(ctx):
 for extension in initial_extensions:
     bot.load_extension(extension)
 
-# async def zipper():
-#     await bot.wait_until_ready()
-#     channel = bot.get_channel('518554813093380098')
-#     server = bot.get_server('369252350927306752')
-#     destination = channel
-#     db = psycopg2.connect(host=config.host,database=config.database, user=config.user, password=config.password)
-#     cursor = db.cursor()
-#     for x in server.channels:
-#         if str(x.type) == "text":
-#             try:
-#                 cursor.execute("SELECT * FROM logs WHERE channel = '"+str(x.id)+"' ORDER BY time;")
-#                 c = cursor.fetchall()
-#                 path = "Just Monika (And Friends) #DAENATAKEOVER/"
-#                 if not os.path.exists(path):
-#                     os.makedirs(path)
-#                 try:
-#                     os.remove("Just Monika (And Friends) #DAENATAKEOVER/"+x.name+".txt")
-#                 except:
-#                     pass
-#                 txt = open("Just Monika (And Friends) #DAENATAKEOVER/"+x.name+".txt","at")
-#                 for x in c:
-#                     todisplay = x[1]
-#                     details = x[3]
-#                     txt.write(details+"\n"+todisplay+"\n\n")
-#                 txt.close()
-#             except Exception as e:
-#                 print(e)
-#     await bot.wait_until_ready()
-#     def zipdir(path, ziph):
-#         # ziph is zipfile handle
-#         for root, dirs, files in os.walk(path):
-#             for file in files:
-#                 ziph.write(os.path.join(root, file))
-#     b = io.BytesIO()
-#     zipf = zipfile.ZipFile(b, mode="w")
-#     zipdir('Just Monika (And Friends) #DAENATAKEOVER/', zipf)
-#     zipf.close()
-#     b.seek(0)
-#     await bot.send_file(channel, fp=b, filename=str(datetime.now().day)+"/"+str(datetime.now().month)+"/"+str(datetime.now().year)+"JMAFLogs.zip")
-#     shutil.rmtree("Just Monika (And Friends) #DAENATAKEOVER/")
-#     db = psycopg2.connect(host=config.host,database=config.database, user=config.user, password=config.password)
-#     cursor = db.cursor()
-#     cursor.execute("truncate logs;")
-#     db.commit()
-#     db.close()
-#     await asyncio.sleep(60*60*24)
+async def zipper():
+    await bot.wait_until_ready()
+    channel = bot.get_channel('518554813093380098')
+    server = bot.get_server('369252350927306752')
+    destination = channel
+    db = psycopg2.connect(host=config.host,database=config.database, user=config.user, password=config.password)
+    cursor = db.cursor()
+    for x in server.channels:
+        if str(x.type) == "text":
+            try:
+                cursor.execute("SELECT * FROM logs WHERE channel = '"+str(x.id)+"' ORDER BY time;")
+                c = cursor.fetchall()
+                path = "Just Monika (And Friends) #DAENATAKEOVER/"
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                try:
+                    os.remove("Just Monika (And Friends) #DAENATAKEOVER/"+x.name+".txt")
+                except:
+                    pass
+                txt = open("Just Monika (And Friends) #DAENATAKEOVER/"+x.name+".txt","at")
+                for x in c:
+                    todisplay = x[1]
+                    details = x[3]
+                    txt.write(details+"\n"+todisplay+"\n\n")
+                txt.close()
+            except Exception as e:
+                print(e)
+    await bot.wait_until_ready()
+    def zipdir(path, ziph):
+        # ziph is zipfile handle
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                ziph.write(os.path.join(root, file))
+    b = io.BytesIO()
+    zipf = zipfile.ZipFile(b, mode="w")
+    zipdir('Just Monika (And Friends) #DAENATAKEOVER/', zipf)
+    zipf.close()
+    b.seek(0)
+    await bot.send_file(channel, fp=b, filename=str(datetime.now().day)+"/"+str(datetime.now().month)+"/"+str(datetime.now().year)+"JMAFLogs.zip")
+    shutil.rmtree("Just Monika (And Friends) #DAENATAKEOVER/")
+    db = psycopg2.connect(host=config.host,database=config.database, user=config.user, password=config.password)
+    cursor = db.cursor()
+    cursor.execute("truncate logs;")
+    db.commit()
+    db.close()
+    await asyncio.sleep(60*60*24)
 
-# bot.loop.create_task(zipper())
+bot.loop.create_task(zipper())
 bot.run(config.token)
