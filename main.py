ce292f57e8aa7b4b890c06c2d8898d51874a5dc6
Mohamed Ali <theforkbomber@gmail.com>
2018-12-21 19:20:53 +0000

    Fixes

--- main.py
+++ main.py
@@ -635,7 +635,7 @@ async def on_message(message):
             message.content = ">os.roleme sayori"
             await bot.process_commands(message)
             return
-        elif len(test) > 1:
+        elif len(test) > 1 and test[0] not in ["Cinnamon bun", "Cinnamon Bun", "cinnamon bun", "cinnamon Bun"]:
             try:
                 ##await bot.wait_until_ready()
                 print(message.channel)
