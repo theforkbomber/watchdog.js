20be9d3c5953be601cae7490b7c5adcb29cf2d97
Mohamed Ali <theforkbomber@gmail.com>
2018-12-24 22:58:36 +0000

    FIXES

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -141,7 +141,7 @@ class Moderation:
                     strike = "strike"
                 else:
                     strike = "strikes"
-                await self.bot.say(f"{user.name} has been warned by {ctx.message.author.name} because:\n{reason}\n{user.name} is on {c[2]} {strike}.")
+                await self.bot.say(f"{user.name} has been warned by {ctx.message.author.name} because:\n{reason}\n{user.name} is on {c[2] + 1} {strike}.")
         db.commit()
         db.close()
 
