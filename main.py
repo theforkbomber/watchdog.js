fc9aaad4dae5cdbb794a54df553fbb29cedcf1d4
theforkbomber <theforkbomber@gmail.com>
2019-01-02 18:07:04 +0000

    Fixes

--- main.py
+++ main.py
@@ -611,7 +611,7 @@ async def on_message(message):
     if message.channel.is_private == True:
         return
 
-    prog = re.compile(r'(https?://)?(www.)?(discord.(gg|io|me|li|link)|discordapp.com/invite)/.+[a-z]')
+    prog = re.compile(r'(https?://)?(www.)?(discord.(gg|io|me|li|link)|discordapp.com/invite)/.+[A-Za-z]')
     line = str(message.content)
     if prog.search(line) and not message.author.server_permissions.manage_roles:
         m = prog.search(line)
