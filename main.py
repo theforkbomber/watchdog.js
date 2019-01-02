c46041842accca42dc59de00152da1cbb98c49ac
theforkbomber <theforkbomber@gmail.com>
2019-01-02 18:10:45 +0000

    Fixes

--- main.py
+++ main.py
@@ -198,6 +198,20 @@ async def on_command_error(error, ctx):
 
 @bot.event
 async def on_message_edit(before, after):
+    prog = re.compile(r'(https?://)?(www.)?(discord.(gg|io|me|li|link)|discordapp.com/invite)/.+[A-Za-z0-9]')
+    line = str(message.content)
+    if prog.search(line) and not message.author.server_permissions.manage_roles:
+        m = prog.search(line)
+        print(f"MATCHED\nUser: {message.author.name}\nInvite: {message.content}")
+        result = m.group(0) if m else ""
+        if result:
+            print(str(result))
+            c = await bot.get_invite(str(result))
+            if c.server.id in ["369252350927306752", "372766620977725441"]:
+                return
+        await bot.delete_message(message)
+        await bot.send_message(message.author, "You aren't permitted to advertise in this server.")
+        return
     try:
         if str(before.channel.type) == "private":
             return
