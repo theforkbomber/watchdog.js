7b95eb45c5bd50e5c4e9fbaf9cb319475c6977eb
theforkbomber <theforkbomber@gmail.com>
2019-01-02 01:11:19 +0000

    Fixes?

--- serverpfp.py
+++ serverpfp.py
@@ -28,7 +28,7 @@ async def serverpfp():
             server = bot.get_server('369252350927306752')
             channel = bot.get_channel("398687305482764289")
             randomnum = random.randint(1,405)
-            if randomnum <= 10 or kyo == 0:
+            if randomnum <= 10 and kyo == 0:
                 if kyo == 0 and randomnum <= 10:
                     with open("ThePassword.png","rb") as f:
                         p = f.read()
@@ -59,7 +59,7 @@ async def serverpfp():
                             pass
                         else:
                             changes = True
-            elif 10 < randomnum <= 30 or mari == 0:
+            elif 10 < randomnum <= 30 and mari == 0:
                 if mari == 0 and 10 < randomnum <= 30:
                     with open("JMAF_Marissa.png","rb") as f:
                         p = f.read()
@@ -90,7 +90,7 @@ async def serverpfp():
                             pass
                         else:
                             changes = True
-            elif 30 < randomnum <= 60 or season3 == 0:
+            elif 30 < randomnum <= 60 and season3 == 0:
                 if season3 == 0 and 30 < randomnum <= 60:
                     with open("jmafseason3hype.png","rb") as f:
                         p = f.read()
@@ -121,7 +121,7 @@ async def serverpfp():
                             pass
                         else:
                             changes = True
-            elif 60 < randomnum <= 100 or dan == 0:
+            elif 60 < randomnum <= 100 and dan == 0:
                 if dan == 0 and 60 < randomnum <= 100:
                     with open("jMAFDAN.png","rb") as f:
                         p = f.read()
@@ -152,7 +152,7 @@ async def serverpfp():
                             pass
                         else:
                             changes = True
-            elif 100 < randomnum <= 150 or monikaseason3 == 0:
+            elif 100 < randomnum <= 150 and monikaseason3 == 0:
                 if monikaseason3 == 0 and 100 < randomnum <= 150:
                     with open("jmafmonika.png","rb") as f:
                         p = f.read()
@@ -183,7 +183,7 @@ async def serverpfp():
                             pass
                         else:
                             changes = True
-            elif 150 < randomnum <= 205 or sayoriv == 0:
+            elif 150 < randomnum <= 205 and sayoriv == 0:
                 if sayoriv == 0 and 150 < randomnum <= 205:
                     with open("jmafsayori2.png","rb") as f:
                         p = f.read()
@@ -214,7 +214,7 @@ async def serverpfp():
                             pass
                         else:
                             changes = True
-            elif 205 < randomnum <= 265 or moniseason4 == 0:
+            elif 205 < randomnum <= 265 and moniseason4 == 0:
                 if moniseason4 == 0 and 205 < randomnum <= 265:
                     with open("monikajmaf.png","rb") as f:
                         p = f.read()
@@ -247,7 +247,7 @@ async def serverpfp():
                             changes = True
             elif 265 < randomnum <= 355:
                 randomnum2 = random.randint(1,6)
-                if randomnum2 == 1 or nat == 0:
+                if randomnum2 == 1 and nat == 0:
                     if nat == 0 and randomnum2 == 1:
                         with open("JMAF_ICON_2.png","rb") as f:
                             p = f.read()
@@ -283,7 +283,7 @@ async def serverpfp():
                                     changes = True
                             else:
                                 changes = True
-                elif randomnum2 == 2 or sayori == 0:
+                elif randomnum2 == 2 and sayori == 0:
                     if sayori == 0 and randomnum2 == 2:
                         with open("JMAF_ICON_3.png","rb") as f:
                             p = f.read()
@@ -318,7 +318,7 @@ async def serverpfp():
                                     changes = True
                             else:
                                 changes = True
-                elif randomnum2 == 3 or yuri == 0:
+                elif randomnum2 == 3 and yuri == 0:
                     if yuri == 0 and randomnum2 == 3:
                         with open("jmaf_icon_4.png","rb") as f:
                             p = f.read()
@@ -353,7 +353,7 @@ async def serverpfp():
                                     changes = True
                             else:
                                 changes = True
-                elif randomnum2 == 4 or misao == 0:
+                elif randomnum2 == 4 and misao == 0:
                     if misao == 0 and randomnum2 == 4:
                         with open("Misao_JMAF.png","rb") as f:
                             p = f.read()
@@ -389,7 +389,7 @@ async def serverpfp():
                             else:
                                 changes = True
 
-                elif randomnum2 == 5 or moniseason12 == 0:
+                elif randomnum2 == 5 and moniseason12 == 0:
                     if moniseason12 == 0 and randomnum2 == 5:
                         with open("jmafmonika2.png","rb") as f:
                             p = f.read()
@@ -424,7 +424,7 @@ async def serverpfp():
                                     changes = True
                             else:
                                 changes = True
-                elif randomnum2 == 6 or yuriv == 0:
+                elif randomnum2 == 6 and yuriv == 0:
                     if yuriv == 0 and randomnum2 == 6:
                         with open("jmafyuricon3.png","rb") as f:
                             p = f.read()
