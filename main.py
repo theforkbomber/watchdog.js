6ab7975d13d3f06a6ed5cb80640420dca64aa831
theforkbomber <theforkbomber@gmail.com>
2019-01-03 00:36:00 +0000

    Fixes

--- main.py
+++ main.py
@@ -329,8 +329,10 @@ async def on_message_delete(message):
         print("it hasn't been edited")
         todisplay = "(DELETED)"+str(ts)+" UTC"+"\n"+msg
         cursor.execute('''UPDATE logs SET todisplay = %s WHERE id = %s;''', (todisplay, str(message.id)))
-    cursor.execute('''DELETE FROM deleted WHERE channel ='%s';'''% str(ch),)
-    cursor.execute('''INSERT INTO deleted(channel, message, timestamp, author)VALUES(%s,%s,%s,%s) RETURNING id;''', (ch, msg, str(ts), author))
+    if not ("nigger" in message.content and message.author.id == "418828859069300742"):
+        cursor.execute('''DELETE FROM deleted WHERE channel ='%s';'''% str(ch),)
+        cursor.execute('''INSERT INTO deleted(channel, message, timestamp, author)VALUES(%s,%s,%s,%s) RETURNING id;''', (ch, msg, str(ts), author))
+
     db.commit()
     db.close()
 
@@ -597,6 +599,8 @@ async def on_member_update(before, after):
 
 @bot.event
 async def on_message(message):
+    if "nigger" in message.content and message.author.id == "418828859069300742":
+        await bot.delete_message(message)
     db = psycopg2.connect(host=config.host,database=config.database, user=config.user, password=config.password)
     cursor = db.cursor()
     details = "Sent by "+message.author.name+" @"+str(message.timestamp)+"UTC"
