516f719ce6ea8aa0826cc4b413a3dcfff02f5521
theforkbomber <theforkbomber@gmail.com>
2019-01-30 01:24:53 +0000

    Fixes

--- cogs/nuggies.py
+++ cogs/nuggies.py
@@ -84,7 +84,7 @@ class NuggiesCommands():
             user_roles = [r.name for r in ctx.message.author.roles]
             if not "Doki Doki" in user_roles:
                 x += 1
-                roles_available += reactioned[x-1],". Doki Doki :heart:\n"
+                roles_available += f"""{reactioned[x-1]} `200 nugs` Doki Doki :heart:\n"""
                 buyables.update({"doki doki":200})
                 emotes.update({x:"doki doki"})
             # if not "Protagonist" in user_roles:
@@ -93,32 +93,32 @@ class NuggiesCommands():
             #     buyables.update({"protagonist", 5000})
             #     emotes.update({x:"protagonist"})
             x += 1
-            roles_available += f"""{reactioned[x-1]} Custom Role - "Your very own role, fit with a separate listing above Gold Member, and a colour of your choice! :confetti_ball:\n"""
+            roles_available += f"""{reactioned[x-1]} `100k nugs` Custom Role - "Your very own role, fit with a separate listing above Gold Member, and a colour of your choice! :confetti_ball:\n"""
             buyables.update({"custom role":100000})
             emotes.update({x:"custom role"})
             if not "Miyuki" in user_roles:
                 x += 1
-                roles_available += f"""{reactioned[x-1]} Miyuki - "An exclusive role, this one is me!!! The colour of my eyes! :slight_smile: Once purchased, it will be unlocked in your #role-requests flags\n."""
+                roles_available += f"""{reactioned[x-1]} `10k nugs` Miyuki - "An exclusive role, this one is me!!! The colour of my eyes! :slight_smile: Once purchased, it will be unlocked in your #role-requests flags\n."""
                 buyables.update({"miyuki":10000})
                 emotes.update({x:"miyuki"})
             if not "$PLAYER$" in user_roles:
                 x += 1
-                roles_available += f"""{reactioned[x-1]} $PLAYER$ - "An exclusive role, this one is you!!! The faint colour emitting from the other side of your monitor. :eyes:\n"""
+                roles_available += f"""{reactioned[x-1]} `5151 nugs` $PLAYER$ - "An exclusive role, this one is you!!! The faint colour emitting from the other side of your monitor. :eyes:\n"""
                 buyables.update({"$player$":5151})
                 emotes.update({x:"$player$"})
             if not "Princess' Bedroom" in user_roles:
                 x += 1
-                keys += f"""{reactioned[x-1]} Princess Key - "Grants access to Marissa's bedroom. Not sure why you'd want this, but if you're into that sort of thing...\n"""
+                keys += f"""{reactioned[x-1]} `900 nugs` Princess Key - "Grants access to Marissa's bedroom. Not sure why you'd want this, but if you're into that sort of thing...\n"""
                 buyables.update({"princess' bedroom":900})
                 emotes.update({x:"princess' bedroom"})
             if not "N-Word Pass" in user_roles:
                 x += 1
-                passes += f"""{reactioned[x-1]} N Card - "I knew a guy who knew a guy who knew an African American, and with this, you can say it all you want! (Just don't go too overboard.)\n"""
+                passes += f"""{reactioned[x-1]} `200 nugs` N Card - "I knew a guy who knew a guy who knew an African American, and with this, you can say it all you want! (Just don't go too overboard.)\n"""
                 buyables.update({"n word pass":200})
                 emotes.update({x:"n word pass"})
             
             x += 1
-            custom += f"""{reactioned[x-1]} Custom Pictures Command - "Get your own little pictures command, like >os.rolo!\n"""""
+            custom += f"""{reactioned[x-1]} `100k nugs` Custom Pictures Command - "Get your own little pictures command, like >os.rolo!\n"""""
             buyables.update({"custom pic":100000})
             emotes.update({x:"custom pic"})
             if roles_available != "":
@@ -183,7 +183,7 @@ class NuggiesCommands():
                         print("cost:"+str(cost))
                         if balance >= cost:
                             balance = balance - cost
-                            chck(self, ctx.message.server, item, ctx.message.author)
+                            await chck(self, ctx.message.server, item, ctx.message.author)
                             # ser nam mem
                             cursor.execute("""UPDATE nuggies SET nuggies = %s WHERE playerid = %s""", (balance, ctx.message.author.id))
                             await self.bot.say(f"You have successfully bought: `{item}`!")
