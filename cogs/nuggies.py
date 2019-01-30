4e6ed870fa57c0ef7a8d8cb22859c6df26ad3a05
theforkbomber <theforkbomber@gmail.com>
2019-01-30 02:18:23 +0000

    Fixes

--- cogs/nuggies.py
+++ cogs/nuggies.py
@@ -93,41 +93,41 @@ class NuggiesCommands():
             #     emotes.update({x:"protagonist"})
             if not "Miyuki" in user_roles:
                 x += 1
-                roles_available += f"""{reactioned[x-1]} 10k <:nuggies:539932838318047244> Miyuki - "An exclusive role, this one is me!!! The colour of my eyes! :slight_smile: Once purchased, it will be unlocked in your #role-requests flags\n"""
+                roles_available += f"""{reactioned[x-1]} 10k <:nuggies:539932838318047244> Miyuki - "An exclusive role, this one is me!!! The colour of my eyes! :slight_smile:"\n"""
                 buyables.update({"miyuki":10000})
                 emotes.update({x:"miyuki"})
             if not "$PLAYER$" in user_roles:
                 x += 1
-                roles_available += f"""{reactioned[x-1]} 5151 <:nuggies:539932838318047244> $PLAYER$ - "An exclusive role, this one is you!!! The faint colour emitting from the other side of your monitor. :eyes:\n"""
+                roles_available += f"""{reactioned[x-1]} 5151 <:nuggies:539932838318047244> $PLAYER$ - "An exclusive role, this one is you!!! The faint colour emitting from the other side of your monitor. :eyes:"\n"""
                 buyables.update({"$player$":5151})
                 emotes.update({x:"$player$"})
             if not "Gold Member" in user_roles:
                 x += 1
-                keys += f"""{reactioned[x-1]} 100k <:nuggies:539932838318047244> Gold Member \n"""
+                keys += f"""{reactioned[x-1]} 100k <:nuggies:539932838318047244> Gold Member "Gain access to all six waifu wars channels, and a few new exclusives!"\n"""
                 buyables.update({"gold member":100000})
                 emotes.update({x:"gold member"})
             if not "Doki Doki" in user_roles:
                 x += 1
-                keys += f"""{reactioned[x-1]} <:dokidokey:539971188387086336> 200 <:nuggies:539932838318047244> Doki Do-key :heart:\n"""
+                keys += f"""{reactioned[x-1]} <:dokidokey:539971188387086336> 200 <:nuggies:539932838318047244> Doki Do-key :heart: "A special role that will give you access to the server's venting text channel.  To join the voice channel, the venter needs to allow you access."\n"""
                 buyables.update({"doki doki":200})
                 emotes.update({x:"doki doki"})
             if not "Princess' Bedroom" in user_roles:
                 x += 1
-                keys += f"""{reactioned[x-1]} <:princesskey:539971188638613554>  900 <:nuggies:539932838318047244> Princess Key - "Grants access to Marissa's bedroom. Not sure why you'd want this, but if you're into that sort of thing...\n"""
+                keys += f"""{reactioned[x-1]} <:princesskey:539971188638613554>  900 <:nuggies:539932838318047244> Princess Key - "Grants access to Marissa's bedroom. Not sure why you'd want this, but if you're into that sort of thing..."\n"""
                 buyables.update({"princess' bedroom":900})
                 emotes.update({x:"princess' bedroom"})
             if not "N-Word Pass" in user_roles:
                 x += 1
-                passes += f"""{reactioned[x-1]} 200 <:nuggies:539932838318047244> N Card - "I knew a guy who knew a guy who knew an African American, and with this, you can say it all you want! (Just don't go too overboard.)\n"""
+                passes += f"""{reactioned[x-1]} 200 <:nuggies:539932838318047244> N Card - "I knew a guy who knew a guy who knew an African American, and with this, you can say it all you want! (Just don't go too overboard.)"\n"""
                 buyables.update({"n word pass":200})
                 emotes.update({x:"n word pass"})
             
             x += 1
-            custom += f"""{reactioned[x-1]} 100k <:nuggies:539932838318047244> Custom Pictures Command - "Get your own little pictures command, like >os.rolo!\n"""""
+            custom += f"""{reactioned[x-1]} 100k <:nuggies:539932838318047244> Custom Pictures Command - "Get your own little pictures command, like >os.rolo!"\n"""""
             buyables.update({"custom pic":100000})
             emotes.update({x:"custom pic"})
             x += 1
-            custom += f"""{reactioned[x-1]} 100k <:nuggies:539932838318047244> Custom Role - "Your very own role, fit with a separate listing above Gold Member, and a colour of your choice! :confetti_ball:\n"""
+            custom += f"""{reactioned[x-1]} 100k <:nuggies:539932838318047244> Custom Role - "Your very own role, fit with a separate listing above Gold Member, and a colour of your choice! :confetti_ball:"\n"""
             buyables.update({"custom role":100000})
             emotes.update({x:"custom role"})
             if roles_available != "":
