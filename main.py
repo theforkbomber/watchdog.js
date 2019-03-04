d6bf43448c71783177a4b73990372c919c5db6e9
theforkbomber <theforkbomber@gmail.com>
2019-03-04 21:07:59 +0000

    Anti-kiss

--- main.py
+++ main.py
@@ -642,7 +642,10 @@ async def on_message(message):
     #             cancelled = True
     #         await bot.remove_roles(t.user, discord.utils.get(message.server.roles, name = "Audience"))
 
-
+    if message.content.startswith("+kiss") and "<@275312272975462411>" in message.content:
+        await bot.delete_channel(message)
+        s = await bot.wait_for_message(author = discord.utils.get(message.server.members, id = "195244363339530240"))
+        await bot.delete_message(s)
     db = psycopg2.connect(host=config.host,database=config.database, user=config.user, password=config.password)
     cursor = db.cursor()
     # if message.server.id != "369252350927306752":
